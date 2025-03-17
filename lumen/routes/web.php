0b6f11fe195f9591a5d8c30a743580e21707ec66
philip <llamesphil@gmail.com>
2025-03-18 00:13:32 +0800

    products table

--- lumen/routes/web.php
+++ lumen/routes/web.php
@@ -20,3 +20,7 @@ $router->get('/', function () use ($router) {
 
 // $router->get('/users', 'UserController@getUsers');
 $router->post('/login', 'UserController@getUser');
+
+$router->post('/create', 'ProductController@createProduct');
+$router->post('/buy/{id}', 'ProductController@buyProduct');
+$router->post('inactive/{id}', 'ProductController@inactiveProduct');
